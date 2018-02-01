d3f818ff410879075920c20b72ee850f3da3c25f
mis1 <imran.imran0@gmail.com>
2018-02-01 13:23:00 +0500

    deleted all files except for dist files. copied files from dist folder of master on root of gh-pages branch.

--- app/index.js
+++
@@ -1,9 +0,0 @@
-import $ from './libs/jquery';
-import Controller from './controllers/Controller';
-import Model from './models/Model';
-
-
-$(()=>{
-	const model = new Model();
-	new Controller(model);
-});
